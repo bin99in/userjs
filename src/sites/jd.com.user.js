12d5e5312aa703baa2dab4d36772ada1f1796eb9
bin99 <sd4zhb@gmail.com>
2020-06-10 22:06:30 +0800

    京东自动登录

--- src/sites/jd.com.user.js
+++ src/sites/jd.com.user.js
@@ -1,9 +1,12 @@
 import SiteConfig from '../classes/SiteConfig.js'
 
+const unsafeDoc = unsafeWindow.document
+
 const config = new Map([
   ['passport.jd.com', new Map([
-    [new RegExp('^/uc/login'), () => {
+    [new RegExp('^(/uc/login)|(/new/login)'), () => {
       getOutQrcode()
+      quickLogin()
     }]
   ])]
 ])
@@ -18,6 +21,25 @@ function getOutQrcode () {
     loginTabShowBtn.click()
     checkLoginBoxIsShown() && clearInterval(timer)
   }, 100)
+}
+
+/** 若用户名已自动填充，则发生交互时立即登录
+    用户没有交互过，脚本无法获取password的value值
+ */
+function quickLogin() {
+  const $ = selector => document.querySelector(selector)
+  const isAutofilled = !!document.querySelector('#loginname')
+  const login = () => isAutofilled && $('#loginsubmit').click()
+  unsafeDoc.addEventListener('mousemove', function(e){
+    login()
+  }, { capture: true})
+  unsafeDoc.addEventListener('click', function(e){
+    login()
+  }, { capture: true})
+  unsafeWindow.addEventListener('keydown', function(e) {
+    const hasPwd = !!$('#nloginpwd').value
+    isAutofilled && hasPwd && $('#loginsubmit').click()
+  }, { capture: true})
 }
 
 export default new SiteConfig(config)
